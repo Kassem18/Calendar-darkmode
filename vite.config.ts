b27322147a84939073c09aade753f44a0ee39159
Kassem <u.u.d.d@hotmail.com>
2026-02-11 14:21:30 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Calendar-darkmode/',   // correct!
-})
+  base: "/Calendar-darkmode/", // correct!
+  server: {
+    port: 5173, // Explicitly set port
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
